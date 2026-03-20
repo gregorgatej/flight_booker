4e5b679965cf9eceb37287264025791f56ce831b
Gregor Gatej <gregor.gatej@gmail.com>
2026-03-20 14:28:38 +0100

    feat: show matching flights and add flight selection form

--- app/helpers/flights_helper.rb
+++ app/helpers/flights_helper.rb
@@ -1,2 +1,7 @@
 module FlightsHelper
+  def search_params_present?
+      params[:departure_airport_id].present? &&
+      params[:arrival_airport_id].present? &&
+      params[:date].present?
+  end
 end

32ce19e64128585ec04d33091a73d4c06f2af662
Gregor Gatej <gregor.gatej@gmail.com>
2026-03-20 13:50:26 +0100

    feat: add flight search form with airport date and passenger filters

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,25 @@
 class FlightsController < ApplicationController
   def index
+    @airports = Airport.order(:code)
+    @flight_dates = Flight.ordered.map { |flight| flight.start_datetime.to_date }.uniq
+
+    @matching_flights = []
+
+    return unless search_params_present?
+
+    date = Date.parse(params[:date])
+
+    @matching_flights = Flight.search(
+      departure_airport_id: params[:departure_airport_id],
+      arrival_airport_id: params[:arrival_airport_id],
+      date: date
+    )
   end
+
+  private
+    def search_params_present?
+      params[:departure_airport_id].present? &&
+      params[:arrival_airport_id].present? &&
+      params[:date].present?
+    end
 end

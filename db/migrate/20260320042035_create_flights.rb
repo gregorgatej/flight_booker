7c3bcb8a24a5c5fce93409a3042a6ce08d7bfc94
Gregor Gatej <gregor.gatej@gmail.com>
2026-03-20 05:27:11 +0100

    feat: add airport and flight models

---
+++ db/migrate/20260320042035_create_flights.rb
@@ -0,0 +1,13 @@
+class CreateFlights < ActiveRecord::Migration[8.1]
+  def change
+    create_table :flights do |t|
+      t.references :departure_airport, null: false, foreign_key: { to_table: :airports }
+      t.references :arrival_airport, null: false, foreign_key: { to_table: :airports }
+      t.datetime :start_datetime
+      t.integer :duration_minutes
+      t.integer :price_cents
+
+      t.timestamps
+    end
+  end
+end

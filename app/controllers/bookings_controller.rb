30e12a7be0c7c5060ff4c5a8bd335f7f160f2eba
Gregor Gatej <gregor.gatej@gmail.com>
2026-03-21 07:31:48 +0100

    feat: add bookings controller with nested passenger params

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,33 @@
+class BookingsController < ApplicationController
+  def new
+    @flight = Flight.find(params[:flight_id])
+    @booking = Booking.new(flight: @flight)
+
+    params[:num_passengers].to_i.times do
+      @booking.passengers.build
+    end
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @fligth = @booking.flight
+
+    if @booking.save
+      redirect_to @booking
+    else
+      render :new, status: unprocessable_entity
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+    def booking_params
+      params.require(:booking).permit(
+        :flight_id,
+        passengers_attributes: [ :name, :email ]
+      )
+    end
+end

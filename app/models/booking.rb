f4166851fd9383272932f0b3d46e97355472e360
Gregor Gatej <gregor.gatej@gmail.com>
2026-03-29 12:03:58 +0200

    feat: allow destroy for nested passenger attributes

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :flight
   has_many :passengers, dependent: :destroy
 
-  accepts_nested_attributes_for :passengers
+  accepts_nested_attributes_for :passengers, allow_destroy: true
 
   validates :flight, presence: true
 end
